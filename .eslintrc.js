1efe8a0cbde4dd63544a5e918088fe51c7ed9029
Joe Maller <joe@joemaller.com>
2021-09-28 22:39:20 -0400

    experimenting with eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,16 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   rules: {
     "sort-imports": ["error", { allowSeparatedGroups: true }],
   },
+  plugins: ["react"],
 };
